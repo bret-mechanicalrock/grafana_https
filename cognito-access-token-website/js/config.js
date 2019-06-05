8d9bb2066c1f10822c119255e8111eda7375035e
Bret Waldow <bret.waldow@mechanicalrock.io>
2019-06-05 10:07:58 +0800

    Remove unused demo web site

--- cognito-access-token-website/js/config.js
+++
@@ -1,5 +0,0 @@
-var UserPoolId = 'ap-southeast-2_hoGKpujjF';
-var ClientId = '2gukln7ue8msn3flij2713r2oq';
-var AWSRegion = 'ap-southeast-2';
-var DomainPrefix = '1d5s30t66sr35i569862681p5p';
-var BaseURL = 'dx2wb59dqijk1.cloudfront.net';
